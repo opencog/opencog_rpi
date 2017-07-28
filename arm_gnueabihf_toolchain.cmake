9dc36223e43de2312ff127b521be206d5a5cc333
Dagiopia <dagiopia@gmail.com>
2017-07-28 09:10:40 +0300

    mistake in tc cmake file

--- arm_gnueabihf_toolchain.cmake
+++ arm_gnueabihf_toolchain.cmake
@@ -4,7 +4,7 @@ SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
 
-SET(CMAKE_FIND_ROOT_PATH /home/$ENV{USER}/$ENV{CC_TC_DIR}/opencog_rpi_toolchain/needed_libs /home/$ENV{USER}/RPI/opencog_rpi_toolchain/opencog_rasp)
+SET(CMAKE_FIND_ROOT_PATH /home/$ENV{USER}/$ENV{CC_TC_DIR}/opencog_rpi_toolchain/needed_libs /home/$ENV{USER}/$ENV{CC_TC_DIR}/opencog_rpi_toolchain/opencog_rasp)
 
 SET(CMAKE_INSTALL_PREFIX /home/$ENV{USER}/$ENV{CC_TC_DIR}/opencog_rpi_toolchain/opencog_rasp/usr/local)
 
