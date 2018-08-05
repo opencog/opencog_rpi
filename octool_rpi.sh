765351836641c39635715018703c62593fa76c8d
Kidist-Abraham <kidistabraham@gmail.com>
2018-08-05 14:15:27 +0300

    install libboost by apt-get instead of dpkg

--- octool_rpi.sh
+++ octool_rpi.sh
@@ -418,9 +418,10 @@ if [ $INSTALL_DEPS ] ; then
 			sudo apt-get install -y $APT_ARGS libboost1.62-dev
 		else
 			#install boost 1.60
-			wget http://144.76.153.5/opencog/libboost-1.55-all-dev-1_armhf.deb
-			sudo dpkg -i libboost-1.55-all-dev-1_armhf.deb
-			rm libboost-1.55-all-dev-1_armhf.deb
+			#wget http://144.76.153.5/opencog/libboost-1.55-all-dev-1_armhf.deb
+			#sudo dpkg -i libboost-1.55-all-dev-1_armhf.deb
+			#rm libboost-1.55-all-dev-1_armhf.deb
+                        sudo apt-get install -y $APT_ARGS libboost1.55-all-dev
 		fi
 	#	install_bdwgc # install bdwgc from source
 	#	install_guile # install guile  from source
